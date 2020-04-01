00b30c7c1a5b62d60f8f7c8fcd07ed7644e18a48
fpicot <fpicot@visiativ.com>
2020-04-01 12:07:56 +0200

    Retrieve queue and message. Be able to remove message from queue.

--- src/main/java/org/example/jms/management/StartupSingletonManagement.java
+++ src/main/java/org/example/jms/management/StartupSingletonManagement.java
@@ -8,10 +8,14 @@ import javax.ejb.Singleton;
 import javax.ejb.Startup;
 import javax.management.MBeanServerConnection;
 import javax.management.MBeanServerInvocationHandler;
+import javax.management.MalformedObjectNameException;
 import javax.management.ObjectName;
 import javax.management.remote.JMXConnector;
 import javax.management.remote.JMXConnectorFactory;
 import javax.management.remote.JMXServiceURL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -23,23 +27,55 @@ public class StartupSingletonManagement {
 	private static final String JMS_ARTEMIS_BROKER_NAME = "default";
 	private static final String HOSTNAME = "localhost";
 	private static final String PORT = "9990";
+	private static final String QUEUE_NAME_DEAD_LETTER = "jms.queue.DLQ";
+	private static final String QUEUE_NAME_EXPIRY = "jms.queue.ExpiryQueue";
+	private static final String MESSAGE_FIELD_MESSAGEID = "messageID";
 	
 	@Schedule(second = "*/10", minute = "*", hour = "*")
 	private void reportTimer() {
 		try {
-			ObjectName on = new ObjectName("org.apache.activemq.artemis:broker=\"" + JMS_ARTEMIS_BROKER_NAME + "\"");
-			try (JMXConnector connector = JMXConnectorFactory.connect(new JMXServiceURL("service:jmx:remote+http://" + HOSTNAME + ":" + PORT), null)) {
+			JMXServiceURL jmxServiceURL = new JMXServiceURL("service:jmx:remote+http://" + HOSTNAME + ":" + PORT);
+			try (JMXConnector connector = JMXConnectorFactory.connect(jmxServiceURL, null)) {
 				MBeanServerConnection mbsc = connector.getMBeanServerConnection();
-				ActiveMQServerControl serverControl = MBeanServerInvocationHandler.newProxyInstance(mbsc, on, ActiveMQServerControl.class, false);
-				for (String queueName : serverControl.getQueueNames()) {
-					ObjectName queueObjectName = new ObjectName(
-						"org.apache.activemq.artemis:broker=\"" + JMS_ARTEMIS_BROKER_NAME + "\",component=addresses,address=\"" + queueName + "\"");
-					QueueControl activeMQQueue = MBeanServerInvocationHandler.newProxyInstance(mbsc, queueObjectName, QueueControl.class, false);
-					LOGGER.log(Level.INFO,"{0} message count: {1}",new Object[]{queueName,activeMQQueue.getMessageCount()});
+				for (QueueControl queueControl : getQueues(mbsc)) {
+					LOGGER.log(Level.INFO, "{0} message count: {1}", new Object[] { queueControl.getName(), queueControl.getMessageCount() });
+					if (QUEUE_NAME_DEAD_LETTER.equals(queueControl.getName()) || QUEUE_NAME_EXPIRY.equals(queueControl.getName())) {
+						cleanQueue(queueControl);
+					}
 				}
 			}
 		} catch (Exception e) {
-			LOGGER.log(Level.SEVERE,"Error retrieving JMS status",e);
+			LOGGER.log(Level.SEVERE, "Error retrieving JMS status", e);
+		}
+	}
+	
+	private List<QueueControl> getQueues(MBeanServerConnection mbsc) throws MalformedObjectNameException {
+		List<QueueControl> queues = new ArrayList<>();
+		ActiveMQServerControl serverControl = getActiveMQServerControl(mbsc);
+		for (String queueName : serverControl.getQueueNames()) {
+			QueueControl queueControl = getQueueControl(mbsc, queueName);
+			queues.add(queueControl);
+		}
+		return queues;
+	}
+	
+	private QueueControl getQueueControl(MBeanServerConnection mbsc, String queueName) throws MalformedObjectNameException {
+		ObjectName queueObjectName = new ObjectName(
+			"org.apache.activemq.artemis:broker=\"" + JMS_ARTEMIS_BROKER_NAME + "\",component=addresses,address=\"" + queueName
+				+ "\",subcomponent=queues,routing-type=\"anycast\",queue=\"" + queueName + "\"");
+		return MBeanServerInvocationHandler.newProxyInstance(mbsc, queueObjectName, QueueControl.class, false);
+	}
+	
+	private ActiveMQServerControl getActiveMQServerControl(MBeanServerConnection mbsc) throws MalformedObjectNameException {
+		ObjectName on = new ObjectName("org.apache.activemq.artemis:broker=\"" + JMS_ARTEMIS_BROKER_NAME + "\"");
+		return MBeanServerInvocationHandler.newProxyInstance(mbsc, on, ActiveMQServerControl.class, false);
+	}
+	
+	private void cleanQueue(QueueControl queueControl) throws Exception {
+		for (Map<String, Object> stringObjectMap : queueControl.listMessages(null)) {
+			long messageId = (Long)stringObjectMap.get(MESSAGE_FIELD_MESSAGEID);
+			queueControl.removeMessage(messageId);
+			LOGGER.log(Level.INFO, "Removing message {0} from {1} queue", new Object[] { messageId, queueControl.getName() });
 		}
 	}
 }
