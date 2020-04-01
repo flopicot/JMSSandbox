df0063cc286f65df98ccd46090c85928426c952f
fpicot <fpicot@visiativ.com>
2020-04-01 12:14:40 +0200

    Simulate error processing message containing the arbitrary "11" value.

--- src/main/java/org/example/jms/messagedrivenbean/consumer/MessageReceiverAsync.java
+++ src/main/java/org/example/jms/messagedrivenbean/consumer/MessageReceiverAsync.java
@@ -1,5 +1,6 @@
 package org.example.jms.messagedrivenbean.consumer;
 
+import org.apache.commons.lang3.NotImplementedException;
 import org.apache.commons.lang3.SerializationUtils;
 import org.example.jms.messagedrivenbean.Resources;
 import org.example.jms.messagedrivenbean.bean.EventMessage;
@@ -28,6 +29,9 @@ public class MessageReceiverAsync implements MessageListener {
 		try {
 			lock.lock();
 			EventMessage eventMessage = SerializationUtils.deserialize(message.getBody(byte[].class));
+			if (eventMessage.getValue().contains("11")) {
+				throw new NotImplementedException("Simulating message processing error : Cannot process message with the 11 value");
+			}
 			//Simulate time to process the message
 			waitCond.await(500, TimeUnit.MILLISECONDS);
 			LOGGER.log(Level.INFO,"{0}-Message received (async): {1} - {2}", new Object[]{MessageReceiverAsync.class.getName(),eventMessage.getType(),eventMessage.getValue()});
