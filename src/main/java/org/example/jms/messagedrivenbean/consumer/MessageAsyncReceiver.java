baef5327442e1b369e2816601353f53187db263e
fpicot <fpicot@visiativ.com>
2020-04-02 10:24:12 +0200

    Add comments

--- src/main/java/org/example/jms/messagedrivenbean/consumer/MessageAsyncReceiver.java
+++ src/main/java/org/example/jms/messagedrivenbean/consumer/MessageAsyncReceiver.java
@@ -16,11 +16,18 @@ import java.util.concurrent.locks.ReentrantLock;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+/**
+ * A MDB (Message Driven Bean) to consume the the queue
+ */
 @MessageDriven(activationConfig = { @ActivationConfigProperty(propertyName = "destinationLookup", propertyValue = Resources.ASYNC_QUEUE),
-	@ActivationConfigProperty(propertyName = "maxSession", propertyValue = "2"),
-	@ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Queue") })
+	@ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Queue"),
+	@ActivationConfigProperty(propertyName = "maxSession", propertyValue = "2") // Limit the number of concurrent instance of message driven
+})
 public class MessageAsyncReceiver implements MessageListener {
+	
 	private static final Logger LOGGER = Logger.getLogger(MessageAsyncReceiver.class.getName());
+	
+	// To simultate message process time
 	final Lock lock = new ReentrantLock();
 	final Condition waitCond = lock.newCondition();
 	
@@ -29,12 +36,13 @@ public class MessageAsyncReceiver implements MessageListener {
 			lock.lock();
 			EventMessage eventMessage = SerializationUtils.deserialize(message.getBody(byte[].class));
 			// Uncomment this to test error processing message
-//			if (eventMessage.getValue().contains("11")) {
-//				throw new NotImplementedException("Simulating message processing error : Cannot process message with the 11 value");
-//			}
+			//			if (eventMessage.getValue().contains("11")) {
+			//				throw new NotImplementedException("Simulating message processing error : Cannot process message with the 11 value");
+			//			}
 			//Simulate time to process the message
 			waitCond.await(500, TimeUnit.MILLISECONDS);
-			LOGGER.log(Level.INFO,"{0}-Message received (async): {1} - {2}", new Object[]{ MessageAsyncReceiver.class.getName(),eventMessage.getType(),eventMessage.getValue()});
+			LOGGER.log(Level.INFO, "{0}-Message received (async): {1} - {2}",
+				new Object[] { MessageAsyncReceiver.class.getName(), eventMessage.getType(), eventMessage.getValue() });
 		} catch (JMSException | InterruptedException e) {
 			throw new IllegalStateException(e);
 		} finally {
