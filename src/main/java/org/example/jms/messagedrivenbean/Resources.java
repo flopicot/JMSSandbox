e0fb84bfd1bd768c3b68a40aec868c29e92a5a57
fpicot <fpicot@visiativ.com>
2020-04-02 09:57:47 +0200

    Use constant

--- src/main/java/org/example/jms/messagedrivenbean/Resources.java
+++ src/main/java/org/example/jms/messagedrivenbean/Resources.java
@@ -4,9 +4,10 @@ import javax.jms.JMSDestinationDefinition;
 import javax.jms.JMSDestinationDefinitions;
 
 @JMSDestinationDefinitions({
-	@JMSDestinationDefinition(name = Resources.ASYNC_QUEUE, /**resourceAdapter = "jmsra",**/ interfaceName = "javax.jms.Queue", destinationName = "asyncQueue", description = "My Async Queue") })
+	@JMSDestinationDefinition(name = Resources.ASYNC_QUEUE, /**resourceAdapter = "jmsra",**/ interfaceName = "javax.jms.Queue", destinationName = Resources.ASYNC_QUEUE_DESTINATION_NAME, description = "My Async Queue") })
 public class Resources {
 	
 	public static final String ASYNC_QUEUE = "java:app/jms/myAsyncQueue";
+	public static final String ASYNC_QUEUE_DESTINATION_NAME = "asyncQueue";
 	
 }
